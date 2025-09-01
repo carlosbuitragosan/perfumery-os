cd393d2f7612db38724503e6a1d5451143aa7652
Carlos Buitrago <letscatchupagain@gmail.com>
2025-09-01 09:10:55 +0100

    chore(materials): sort materials taxonomy arrays alphabetically (#3).

--- config/materials.php
+++ config/materials.php
@@ -1,8 +1,18 @@
 <?php
 
+$families = ['citrus', 'floral', 'herbal', 'woody', 'resinous', 'spicy', 'green', 'fruity', 'balsamic', 'animalic', 'gourmand', 'leathery', 'smoky', 'marine'];
+$functions = ['fixative', 'modifier'];
+$safety = ['phototoxic', 'toxic', 'irritant', 'allergenic', 'sensitizer'];
+$effects = ['calming', 'focus', 'uplifting', 'grounding', 'sedative', 'aphrodisiac', 'stimulant'];
+
+sort($families);
+sort($functions);
+sort($safety);
+sort($effects);
+
 return [
-    'families' => ['citrus', 'floral', 'herbal', 'woody', 'resinous', 'spicy', 'green', 'fruity', 'balsamic', 'animalic', 'gourmand', 'leathery', 'smoky', 'marine'],
-    'functions' => ['fixative', 'modifier'],
-    'safety' => ['phototoxic', 'toxic', 'irritant', 'allergenic', 'sensitizer'],
-    'effects' => ['calming', 'focus', 'uplifting', 'grounding', 'sedative', 'aphrodisiac', 'stimulant'],
+    'families' => $families,
+    'functions' => $functions,
+    'safety' => $safety,
+    'effects' => $effects,
 ];
